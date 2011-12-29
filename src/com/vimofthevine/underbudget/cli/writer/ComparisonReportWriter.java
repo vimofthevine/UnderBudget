284a88ef6c3595c8ed332256923ee1551329dd8d
vimofthevine <kyletreubig@gmail.com>
2011-12-29 15:35:02 -0500

    Added transactions to comparison report

--- src/com/vimofthevine/underbudget/cli/writer/ComparisonReportWriter.java
+++ src/com/vimofthevine/underbudget/cli/writer/ComparisonReportWriter.java
@@ -1,9 +1,11 @@
 package com.vimofthevine.underbudget.cli.writer;
 
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
 
 import com.vimofthevine.underbudget.analysis.AnalysisResults;
 import com.vimofthevine.underbudget.estimates.Estimate;
+import com.vimofthevine.underbudget.transactions.Transaction;
 import com.vimofthevine.underbudget.util.FormatHelper;
 
 /**
@@ -118,10 +120,119 @@ public class ComparisonReportWriter {
 		
 		stream.println(buffer.toString());
 		
+		if (verbose)
+		{
+			writeTransactions(stream, estimate, level);
+		}
+		
 		// Print out estimate sub-tree
 		for (int i=0; i<estimate.getChildCount(); i++)
 		{
 			writeEstimate(stream, estimate.getChildAt(i), level + 1);
+		}
+	}
+	
+	/**
+	 * Writes the transactions for the given estimate to the
+	 * given output stream at the given indentation level
+	 * 
+	 * @param stream   print stream on which to write
+	 * @param estimate estimate whose transactions are to be written
+	 * @param level    indentation level
+	 */
+	protected void writeTransactions(PrintStream stream, Estimate estimate, int level)
+	{
+		for (Transaction transaction : estimate.getTransactions())
+		{
+			StringBuffer buffer = new StringBuffer();
+			
+			// Add indentation
+			for (int i=0; i<(level + 1); i++)
+			{
+				buffer.append("  ");
+			}
+			
+			int indent = 2 * (level + 1);
+			
+			buffer.append("+");
+			
+			// Add date (8 chars)
+			SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yy");
+			buffer.append(dateFormat.format(transaction.date));
+			
+			// Separator (1 char)
+			buffer.append(" ");
+			
+			// Add payee (16 chars)
+			if (transaction.payee.length() > 16)
+			{
+				buffer.append(transaction.payee.substring(0, 16));
+			}
+			else
+			{
+				buffer.append(transaction.payee);
+			}
+			
+			// Add filler
+			for (int i=0; i<(16 - transaction.payee.length()); i++)
+			{
+				buffer.append(" ");
+			}
+			
+			// Separator (1 char)
+			buffer.append(" ");
+			
+			// Add memo (16 chars)
+			if (transaction.memo.length() > 16)
+			{
+				buffer.append(transaction.memo.subSequence(0, 16));
+			}
+			else
+			{
+				buffer.append(transaction.memo);
+			}
+			
+			// Add filler
+			for (int i=0; i<(16 - transaction.memo.length()); i++)
+			{
+				buffer.append(" ");
+			}
+			
+			// Separator (1 char)
+			buffer.append(" ");
+			
+			// Add deposit (24 chars)
+			String deposit = transaction.deposit.name;
+			if (deposit.length() > (24 - indent))
+			{
+				buffer.append(deposit.substring(0, (24 - indent)));
+			}
+			else
+			{
+				buffer.append(deposit);
+			}
+			
+			// Add filler
+			for (int i=0; i<(24 - indent - deposit.length()); i++)
+			{
+				buffer.append(" ");
+			}
+			
+			// Separator (1 char)
+			buffer.append(" ");
+			
+			// Add amount (10 chars)
+			String amount = FormatHelper.formatCurrency(transaction.value);
+			if (amount.length() > 10)
+			{
+				buffer.append(amount.subSequence(0, 10));
+			}
+			else
+			{
+				buffer.append(amount);
+			}
+			
+			stream.println(buffer.toString());
 		}
 	}
 	
