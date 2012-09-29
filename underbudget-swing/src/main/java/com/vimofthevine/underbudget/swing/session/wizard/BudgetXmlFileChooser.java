a9f8864a7731151ff37a113e7dd1383f59684d01
Kyle Treubig <kyle@vimofthevine.com>
2012-09-29 13:32:38 -0400

    Directories of the last opened budget files are saved in preferences.

--- underbudget-swing/src/main/java/com/vimofthevine/underbudget/swing/session/wizard/BudgetXmlFileChooser.java
+++ underbudget-swing/src/main/java/com/vimofthevine/underbudget/swing/session/wizard/BudgetXmlFileChooser.java
@@ -16,6 +16,8 @@
 
 package com.vimofthevine.underbudget.swing.session.wizard;
 
+import java.io.File;
+
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
 
@@ -28,8 +30,10 @@ class BudgetXmlFileChooser extends JFileChooser {
 	
 	/**
 	 * Constructs a new GnuCash XML file chooser.
+	 * 
+	 * @param directory starting directory
 	 */
-	BudgetXmlFileChooser()
+	BudgetXmlFileChooser(String directory)
 	{
 		// Add the accept-all file filter
 		setAcceptAllFileFilterUsed(true);
@@ -38,6 +42,13 @@ class BudgetXmlFileChooser extends JFileChooser {
 		FileFilter fileFilter = new BudgetXmlFileFilter();
 		addChoosableFileFilter(fileFilter);
 		setFileFilter(fileFilter);
+		
+		// Set directory
+		File dir = new File(directory);
+		if (dir.exists())
+		{
+			setCurrentDirectory(dir);
+		}
 	}
 
 }
