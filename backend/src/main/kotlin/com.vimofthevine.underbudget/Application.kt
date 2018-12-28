7e913234cf3a56ba5ab4adec63b8f7d1ed0d2532
Kyle Treubig <kyle@vimofthevine.com>
2018-12-28 10:22:32 -0500

    Add database connection

--- backend/src/main/kotlin/com.vimofthevine.underbudget/Application.kt
+++ backend/src/main/kotlin/com.vimofthevine.underbudget/Application.kt
@@ -5,7 +5,16 @@ import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
+import org.jetbrains.exposed.sql.Database
+
+val Application.dbUrl get() = environment.config.property("database.url").getString()
+val Application.dbDriver get() = environment.config.property("database.driver").getString()
+val Application.dbUser get() = environment.config.property("database.user").getString()
+val Application.dbPassword get() = environment.config.property("database.password").getString()
+
 fun Application.main() {
+    val db = Database.connect(url = dbUrl, driver = dbDriver, user = dbUser, password = dbPassword)
+    
     routing {
         get("/") {
             call.respondText("Hello, world!", ContentType.Text.Html)
