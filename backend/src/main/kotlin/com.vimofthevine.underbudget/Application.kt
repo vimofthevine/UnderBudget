c5e9b5eab39f8719a4f21a5229aa19eb0e3b6ea3
Kyle Treubig <kyle@vimofthevine.com>
2018-12-28 08:12:07 -0500

    Setup ktor as main class

--- backend/src/main/kotlin/com.vimofthevine.underbudget/Application.kt
+++ backend/src/main/kotlin/com.vimofthevine.underbudget/Application.kt
@@ -1,32 +1,14 @@
 package com.vimofthevine.underbudget
 
-import java.util.Locale
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
-import javax.money.Monetary
-import javax.money.format.AmountFormatQueryBuilder
-import javax.money.format.MonetaryFormats
-
-import org.javamoney.moneta.Money
-import org.javamoney.moneta.format.CurrencyStyle
-
-fun main(args: Array<String>) {
-    println("UnderBudget v0.0.1")
-    
-    val usd = Monetary.getCurrency("USD")
-    val amount = Money.of(1200.75, usd)
-    println("Currency is $usd")
-    println("Amount is $amount")
-    
-    val diff = amount.subtract(Money.of(744.30, usd))
-    println("Diff is $diff")
-    
-    val formatter = MonetaryFormats.getAmountFormat(
-        AmountFormatQueryBuilder.of(Locale.US)
-            .set(CurrencyStyle.SYMBOL)
-            .build())
-    println("Formatted is ${formatter.format(diff)}")
-    
-    for (curr in Monetary.getCurrencies()) {
-        println("Currency $curr (${curr.getNumericCode()})")
+fun Application.main() {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!", ContentType.Text.Html)
+        }
     }
 }
---
+++ backend/src/main/kotlin/com.vimofthevine.underbudget/CurrencyTest.kt
@@ -0,0 +1,32 @@
+package com.vimofthevine.underbudget
+
+import java.util.Locale
+
+import javax.money.Monetary
+import javax.money.format.AmountFormatQueryBuilder
+import javax.money.format.MonetaryFormats
+
+import org.javamoney.moneta.Money
+import org.javamoney.moneta.format.CurrencyStyle
+
+fun main(args: Array<String>) {
+    println("UnderBudget v0.0.1")
+    
+    val usd = Monetary.getCurrency("USD")
+    val amount = Money.of(1200.75, usd)
+    println("Currency is $usd")
+    println("Amount is $amount")
+    
+    val diff = amount.subtract(Money.of(744.30, usd))
+    println("Diff is $diff")
+    
+    val formatter = MonetaryFormats.getAmountFormat(
+        AmountFormatQueryBuilder.of(Locale.US)
+            .set(CurrencyStyle.SYMBOL)
+            .build())
+    println("Formatted is ${formatter.format(diff)}")
+    
+    for (curr in Monetary.getCurrencies()) {
+        println("Currency $curr (${curr.getNumericCode()})")
+    }
+}
