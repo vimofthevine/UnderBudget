de9db4b1773484f6e9dfb962e60bb4b31d0be7f1
Kyle Treubig <kyle@vimofthevine.com>
2019-02-02 12:39:31 -0500

    Add dashboard and ledgers to app drawer

--- web/src/components/Drawer/AppDrawer.js
+++ web/src/components/Drawer/AppDrawer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AccountIcon from '@material-ui/icons/AccountBalance'
+import DashboardIcon from '@material-ui/icons/Dashboard'
 import Divider from '@material-ui/core/Divider'
 import EnvelopeIcon from '@material-ui/icons/Mail'
 import ExpenseIcon from '@material-ui/icons/ShoppingCart'
 import IncomeIcon from '@material-ui/icons/MonetizationOn'
+import LedgerIcon from '@material-ui/icons/Folder'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
@@ -15,6 +17,17 @@ class AppDrawer extends React.Component {
   render() {
     return(
       <Drawer {...this.props}>
+        <List>
+          <ListItem button key="Dashboard">
+            <ListItemIcon><DashboardIcon /></ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem button key="Ledgers">
+            <ListItemIcon><LedgerIcon /></ListItemIcon>
+            <ListItemText primary="Ledgers" />
+          </ListItem>
+        </List>
+        <Divider />
         <List>
           <ListItem button key="Accounts">
             <ListItemIcon><AccountIcon /></ListItemIcon>
