388345d01e1686fd14effd8f9838d075ebc96b7c
Kyle Treubig <kyle@vimofthevine.com>
2019-08-03 22:36:38 +0000

    Add link to register on login page

--- web/src/pages/Login/Login.js
+++ web/src/pages/Login/Login.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Snackbar } from '@material-ui/core';
+import { push } from 'connected-react-router';
+import { Button, Snackbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import NoNavPage from '../../components/NoNavPage/NoNavPage';
 import UserLoginForm from '../../components/UserLoginForm/UserLoginForm';
 import useAuthRequired from '../../hooks/useAuthRequired';
 import { dismissAuthError } from '../../state/ducks/auth';
 
-const Login = ({ authError, onDismissError }) => {
+const useStyles = makeStyles(theme => ({
+  signup: {
+    marginTop: theme.spacing(3),
+    width: '100%',
+  },
+}));
+
+const Login = ({ authError, onDismissError, onSignUp }) => {
   useAuthRequired(false, '/');
+  const classes = useStyles();
+
   return (
-    <NoNavPage
-      avatar={<LockOutlinedIcon />}
-      title='Sign in'
-    >
+    <NoNavPage avatar={<LockOutlinedIcon />} title='Sign in'>
       <UserLoginForm />
+
+      <div className={classes.signup}>
+        <Typography variant='body1' align='right'>
+          Don&lsquo;t have an account?
+          {' '}
+          <Button color='secondary' disableRipple onClick={onSignUp}>
+            Sign up!
+          </Button>
+        </Typography>
+      </div>
+
       <Snackbar
         autoHideDuration={3000}
         message={authError}
@@ -29,6 +48,7 @@ const Login = ({ authError, onDismissError }) => {
 Login.propTypes = {
   authError: PropTypes.string.isRequired,
   onDismissError: PropTypes.func.isRequired,
+  onSignUp: PropTypes.func.isRequired,
 };
 
 const mapState = state => ({
@@ -37,6 +57,10 @@ const mapState = state => ({
 
 const mapDispatch = dispatch => ({
   onDismissError: () => dispatch(dismissAuthError()),
+  onSignUp: () => dispatch(push('/register')),
 });
 
-export default connect(mapState, mapDispatch)(Login);
+export default connect(
+  mapState,
+  mapDispatch,
+)(Login);
